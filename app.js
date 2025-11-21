0862f5ee5ed3f639b1b88710ac5307c9e3a686e3
Pixel Lab NI <pixellabni@outlook.com>
2025-11-21 18:34:18 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -359,8 +359,8 @@ async function fetchSinglePost() {
 
         if (post.youtubeUrl?.includes('embed')) {
             html += `
-                <div class="video-container" style="margin-top: 40px; position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; border-radius: 12px;">
-                    <iframe src="${post.youtubeUrl}" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border:0;" allowfullscreen></iframe>
+                <div class="video-container">
+                    <iframe src="${post.youtubeUrl}" allowfullscreen></iframe>
                 </div>
             `;
         }
